9bf48d5edf3a2795f2db818a172f14e7af643be5
cwtuan <cwtuan@gmail.com>
2014-08-07 16:13:27 +0000

    modulize

--- locale.js
+++ locale.js
@@ -18,144 +18,180 @@ limitations under the License.
  * Ex: Locale.getMsg("Hi, My name is {0}. My email is {1}", "Tony", "tony@example.com");
  * It will show "Hi, My name is Tony. My email is tony@example.com".
  */
-var Locale = {
-    langUrls: ['langs/lang_en_US.properties', 'langs/lang_zh_TW.properties'],
-    hasKey: function(key) {
-        return this.map[key] != null;
-    },
-    getMsg: function(key) {
-        if(this.map[key]) {
-            var args = [],
-                i;
-            if(arguments.length == 1) {
-                return this.map[key];
-            } else { // >1
-                for(i = 1; i < arguments.length; i++) {
-                    args.push(arguments[i]);
-                }
-                return this.map[key].replace(/\{(\d+)\}/g, function(m, i) {
-                    return args[i];
-                });
-            }
-        }
-        return key + '.UNDEFINED';
-        // return this.map[key] ? this.map[key] : key + '.undefined';
-    },
-    setLanguage: function(lang) {        
-        this.lang = lang;        
-    },
-    /**
-     * save language in cookie
-     */
-    saveLanguage: function(lang) {
-        this._setCookie('lang', lang);
-    },
-    load: function() {
-        var me = this;
-        this.map = {};
-        var xmlhttp;
-        if(window.XMLHttpRequest) {
-            // for IE7+, Firefox, Chrome, Opera, Safari
-            xmlhttp = new XMLHttpRequest();
-        } else {
-            // for IE6, IE5
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xmlhttp.onreadystatechange = function() {
-            if(xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                var lines = xmlhttp.responseText.split(/\r\n|\r|\n/g);
-                for(var i = 0; i < lines.length; ++i) {
-                    // console.log(' lines[i]', lines[i]);
-                    var first = lines[i].indexOf('=');
-                    var key = lines[i].substr(0, first);
-                    var value = lines[i].substr(first + 1);
-                    if(key && value) {
-                        value = value.replace(/\r/g, '');
-                        value = value.replace(/\n/g, '');
-                        me.map[key] = value;
-                    }
-                }
-                // console.info('locale loaded');
-                // console.log('this.map',this.map);
-            }
-        };
-        // Preventing Open Redirection Attacks
-        var lang = me.getLanguage();
-        for(var i = 0, len = me.langUrls.length; i < len; i++) {
-            if(me.langUrls[i].indexOf(lang) !== -1) {
-                xmlhttp.open("GET", me.langUrls[i] + '?' + this._getRandParam(), false);
-                xmlhttp.send();
-                return;
-            }
-        }
-        // if preferred language is not listed in langUrls, just use the first one
-        xmlhttp.open("GET", me.langUrls[0] + '?' + this._getRandParam(), false);
-        xmlhttp.send();
-    },
-    _setCookie: function(name, value) {
-        document.cookie = name + "=" + escape(value);
-    },
-    _getCookie: function(name) {
-        var arg = name + "=",
-            alen = arg.length,
-            clen = document.cookie.length,
-            i = 0,
-            j = 0;
-        while(i < clen) {
-            j = i + alen;
-            if(document.cookie.substring(i, j) == arg) {
-                return this._getCookieVal(j);
-            }
-            i = document.cookie.indexOf(" ", i) + 1;
-            if(i === 0) {
-                break;
-            }
-        }
-        return null;
-    },
-    _getCookieVal: function(offset) {
-        var endstr = document.cookie.indexOf(";", offset);
-        if(endstr == -1) {
-            endstr = document.cookie.length;
-        }
-        return unescape(document.cookie.substring(offset, endstr));
-    },
-    getLanguage: function() {
-        // 0. setLanguage() 1.url 2. cookies 3. _guessLanguage 4. default (the first lang)
-        var language = this.lang || this._getQueryParam(location.search, 'lang') || this._getCookie('lang') || this._guessLanguage();
-        return this._formatLang(language);
-    },
-    _guessLanguage: function() {
-        return(navigator.language || navigator.browserLanguage || navigator.userLanguage);
-    },
-    // _buildURL : function() {
-    // // Ex: "app/locale/Application-en_US.txt?_dc=1378102793941"
-    // return this.path + this.prefix + '-' + this.getLanguage() + this.extension + '?' + this._getRandParam();
-    // },
-    _getRandParam: function() {
-        return '_rand=' + (new Date()).getTime();
-    },
-    _formatLang: function(lang) {
-        var lang = lang.split('_');
-        if(lang.length == 1) {
-            lang = lang[0].split('-');
-        }
-        return lang[0].toLowerCase() + '_' + lang[1].toUpperCase();
-    },
-    /**
-     * _getQueryParam('/?lang=zh-TW&debug=true', 'lang') will return 'zh-TW'
-     */
-    _getQueryParam: function(queryString, target) {
-        queryString = location.search.split('?')[1];
-        if(queryString) {
-            var params = queryString.split('&');
-            if(params) {
-                for(var i = 0; i < params.length; ++i) {
-                    var param = params[i].split('=');
-                    if(param[0] === target) return param[1];
-                }
-            }
-        }
-        return null;
-    }
-};
+
+var Locale = (function() {
+	"use strict";
+
+
+	// interface
+	var Module = {
+		setLanguageUrls: setLanguageUrls,
+		getMsg: getMsg,
+		useLanguage: useLanguage,
+		// save language in cookie
+		saveLanguage: saveLanguage,
+		hasKey: hasKey,
+		load: load
+
+	};
+
+
+
+
+
+	function setLanguageUrls(langUrls) {
+		this.langUrls = langUrls;
+	}
+
+	function getMsg(key) {
+		if (this.map[key]) {
+			var args = [],
+				i;
+			if (arguments.length == 1) {
+				return this.map[key];
+			}
+			else { // >1
+				for (i = 1; i < arguments.length; i++) {
+					args.push(arguments[i]);
+				}
+				return this.map[key].replace(/\{(\d+)\}/g, function(m, i) {
+					return args[i];
+				});
+			}
+		}
+		return key + '.UNDEFINED';
+		// return this.map[key] ? this.map[key] : key + '.undefined';
+	}
+
+	function useLanguage(lang) {
+		this.lang = lang;
+	}
+
+	function saveLanguage(lang) {
+		_setCookie('lang', lang);
+	}
+
+
+
+	function hasKey(key) {
+		return this.map[key] !== null;
+	}
+
+
+	function load() {
+		var me = this;
+		this.map = {};
+		var xmlhttp;
+		if (window.XMLHttpRequest) {
+			// for IE7+, Firefox, Chrome, Opera, Safari
+			xmlhttp = new XMLHttpRequest();
+		}
+		else {
+			// for IE6, IE5
+			xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		xmlhttp.onreadystatechange = function() {
+			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+				var lines = xmlhttp.responseText.split(/\r\n|\r|\n/g);
+				for (var i = 0; i < lines.length; ++i) {
+					var first = lines[i].indexOf('=');
+					var key = lines[i].substr(0, first);
+					var value = lines[i].substr(first + 1);
+					if (key && value) {
+						value = value.replace(/\r/g, '');
+						value = value.replace(/\n/g, '');
+						me.map[key] = value;
+					}
+				}
+			}
+		};
+		// Preventing Open Redirection Attacks
+		var lang = _getLanguage.call(this); 
+		for (var i = 0, len = this.langUrls.length; i < len; i++) {
+			if (this.langUrls[i].indexOf(lang) !== -1) {
+				xmlhttp.open("GET", this.langUrls[i] + '?' + _getRandParam(), false);
+				xmlhttp.send();
+				return;
+			}
+		}
+		// if preferred language is not listed in langUrls, just use the first one
+		xmlhttp.open("GET", me.langUrls[0] + '?' + _getRandParam(), false);
+		xmlhttp.send();
+	}
+
+	function _setCookie(name, value) {
+		document.cookie = name + "=" + escape(value);
+	}
+
+	function _getCookie(name) {
+		var arg = name + "=",
+			alen = arg.length,
+			clen = document.cookie.length,
+			i = 0,
+			j = 0;
+		while (i < clen) {
+			j = i + alen;
+			if (document.cookie.substring(i, j) == arg) {
+				return _getCookieVal.call(this, j);
+			}
+			i = document.cookie.indexOf(" ", i) + 1;
+			if (i === 0) {
+				break;
+			}
+		}
+		return null;
+	}
+
+	function _getCookieVal(offset) {
+		var endstr = document.cookie.indexOf(";", offset);
+		if (endstr == -1) {
+			endstr = document.cookie.length;
+		}
+		return unescape(document.cookie.substring(offset, endstr));
+	}
+
+
+	function _getLanguage() {
+
+		// 0. setLanguage() 1.url 2. cookies 3. _guessLanguage 4. default (the first lang)
+		var language = this.lang || _getQueryParam(location.search, 'lang') || _getCookie('lang') || _guessLanguage();
+		return _formatLang(language);
+	}
+
+
+	function _guessLanguage() {
+		return (navigator.language || navigator.browserLanguage || navigator.userLanguage);
+	}
+
+	function _getRandParam() {
+		return '_rand=' + (new Date()).getTime();
+	}
+
+	function _formatLang(lang) {
+		lang = lang.split('_');
+		if (lang.length == 1) {
+			lang = lang[0].split('-');
+		}
+		return lang[0].toLowerCase() + '_' + lang[1].toUpperCase();
+	}
+
+
+	// _getQueryParam('/?lang=zh-TW&debug=true', 'lang') will return 'zh-TW'
+
+	function _getQueryParam(queryString, target) {
+		queryString = location.search.split('?')[1];
+		if (queryString) {
+			var params = queryString.split('&');
+			if (params) {
+				for (var i = 0; i < params.length; ++i) {
+					var param = params[i].split('=');
+					if (param[0] === target) return param[1];
+				}
+			}
+		}
+		return null;
+	}
+
+	return Module;
+
+})();
