6cac92ea1d46d0111db0a354f22a45539efe2121
cwtuan <cwtuan@gmail.com>
2014-08-14 15:41:02 +0000

    rename to  min

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,20 @@
 // including plugins
 var gulp = require('gulp'),
-    uglify = require("gulp-uglify");
+    uglify = require("gulp-uglify"),
+    rename = require("gulp-rename");;
 
 // task
 gulp.task('minify-js', function() {
-    gulp.src('./locale.js') 
-    .pipe(uglify()).pipe(gulp.dest('./locale-min.js'));
+
+    gulp.src('./locale.js').
+    pipe(uglify()).
+    pipe(rename('locale-min.js')).
+    pipe(gulp.dest('./dist'));
+
+   
 });
+
+//   gulp.src('./dist/assets/css/main.css')
+//         .pipe(minifycss())
+//         .pipe(rename('main.min.css'))
+//         .pipe(gulp.dest('./dist/assets/css/'));
