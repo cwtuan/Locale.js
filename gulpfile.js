e43e9520cad7283621e8dad9c3dfa6f160b8084b
cwtuan <cwtuan@gmail.com>
2014-09-22 12:14:07 +0000

    build js with license

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,30 @@
 // including plugins
 var gulp = require('gulp'),
     uglify = require("gulp-uglify"),
-    rename = require("gulp-rename");;
+    rename = require("gulp-rename"),
+    fs = require('fs'),
+    concat = require("gulp-concat"),
+    header = require("gulp-header");
 
-// task
+var getLicense = function() {
+    return '' + fs.readFileSync('LICENSE');
+};
+var getVersion = function() {
+    return fs.readFileSync('VERSION');
+};
+
 gulp.task('minify-js', function() {
+    var version = getVersion(),
+        proudctionName = 'locale-' + version + '.min.js',
+        dest = './example/js',
+        src = './locale.js';
 
-    gulp.src('./locale.js').
-    pipe(uglify()).
-    pipe(rename('locale-1\.0\.min\.js')).
-    pipe(gulp.dest('./example//js'));
-
-   
+    gulp.src(src) // path to your files
+        .pipe(rename(proudctionName))
+        .pipe(uglify())
+        .pipe(header(getLicense(), {
+            version: version,
+            build: (new Date()).toUTCString()
+        }))
+        .pipe(gulp.dest(dest));
 });
